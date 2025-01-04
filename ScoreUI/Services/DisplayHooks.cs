6eb1b0f73f5e7a6b6580b8e2a7770627e6db5a5e
dumiensky <dumiensky@icloud.com>
2025-01-04 13:04:35 +0100

    CurrentDuel tracking

--- ScoreUI/Services/DisplayHooks.cs
+++ ScoreUI/Services/DisplayHooks.cs
@@ -7,11 +7,17 @@ namespace ScoreUI.Services;
 public class DisplayHooks : IDisplayHooks
 {
 	public event EventHandler<CurrentMatchChangedEventArgs>? CurrentMatchChanged;
+	public event EventHandler<CurrentDuelChangedEventArgs>? CurrentDuelChanged; 
 	public event EventHandler<Tournament>? TournamentUpdated; 
 
 	public void SetCurrentMatch(Guid tournamentId, Match? match)
 	{
 		CurrentMatchChanged?.Invoke(this, new(tournamentId, match));
+	}
+
+	public void SetCurrentDuel(Guid matchId, MultiDuelMatch.Duel? duel)
+	{
+		CurrentDuelChanged?.Invoke(this, new(matchId, duel));
 	}
 
 	public void SendTournamentUpdated(Tournament tournament)
--- ScoreUI/Services/Interfaces/IDisplayHooks.cs
+++ ScoreUI/Services/Interfaces/IDisplayHooks.cs
@@ -6,7 +6,9 @@ namespace ScoreUI.Services.Interfaces;
 public interface IDisplayHooks
 {
 	event EventHandler<CurrentMatchChangedEventArgs>? CurrentMatchChanged;
+	event EventHandler<CurrentDuelChangedEventArgs>? CurrentDuelChanged;
 	event EventHandler<Tournament>? TournamentUpdated;
 	void SetCurrentMatch(Guid tournamentId, Match? match);
+	void SetCurrentDuel(Guid matchId, MultiDuelMatch.Duel? duel);
 	void SendTournamentUpdated(Tournament tournament);
 }
